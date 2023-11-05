bbcec15271c7de10fef069741e67d480fc5b3898
atucci <alessiotucci33@gmail.com>
2023-11-05 15:43:59 +0100

    the tester is looking nice

--- tester.pl
+++ tester.pl
@@ -18,8 +18,12 @@ my $reverse = color('reverse');
 my $reset = color('reset');
 
 # Absolute path to the directory containing your program
-# my $dir = "/path/to/your/program";
 my $dir = "/Users/atucci/Desktop/gitPhiloMioafter/philo";
+
+# Command to be executed
+my $cmd = File::Spec->catfile($dir, "philo") . ' && echo "Press enter to continue"';
+
+
 # Change to the specified directory
 chdir $dir or die $red,"Cannot change to directory $dir: $!";
 
@@ -28,12 +32,33 @@ system("make") == 0 or die "Failed to run make: $!";
 
 # Check if the 'philo' executable exists
 if (-e "philo") {
-    print $green, "The 'philo' executable exists, you can start testing.\n";
+    print $green, "\n\nThe 'philo' executable exists, you can start testing.\n\n\n";
 } else {
     die $red, "The 'philo' executable does not exist.\n";
 }
 
-# Open a new Terminal window and run the 'philo' program
-my $cmd = File::Spec->catfile($dir, "philo") . ' && echo "Press enter to continue"';
-system("osascript -e 'tell app \"Terminal\" to do script \"$cmd\"'");
+
+sub test_philo {
+    my @commands = (
+        './philo',
+        './philo 2',
+        './philo 2 200',
+        './philo 2 300 230',
+        './philo 2 400 200 20abc'
+    );
+
+    foreach my $command (@commands) {
+        print $cyan, "Running command: $command\n";
+        my $output = `$command 2>&1`; # Capture both STDOUT and STDERR
+        if ($? == 0) {
+            print $green, "Command succeeded, but was expected to fail.\n";
+        } else {
+            print $green, "Command failed as expected. Error message: $output\n";
+        }
+    }
+}
+
+# Call the test_philo subroutine
+test_philo();
+
 
