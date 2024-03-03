8eeecd81bf24b9a2f2110ff2cfd19da08cc1adef
Alessio Tucci <alessiotucci33@gmail.com>
2024-03-03 13:07:31 +0100

    bing ai fixed it for me

--- get_requestScript.pl
+++ get_requestScript.pl
@@ -5,14 +5,14 @@ use warnings;
 require IO::Socket; ##what is this?
 
 my $url = 'www.google.com';
-
+my $resource = '/'; #root of the website
 my $socket = IO::Socket::INET->new(
   			Proto		=> 'tcp',
 			PeerAddr	=> $url,
 			PeerPort	=> 80) || die "[!] Cannot connect ...\n";
 
 print "Connection to $url\n";
-print $socket "GET $url HTTP/1.0\n\n";
+print $socket "GET $resource HTTP/1.0\n\n";
 $socket->recv(my $data, 1024); 
 print "$data \n";
 close ($socket);
