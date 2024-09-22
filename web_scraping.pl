0b21c05261381616c2a2530d27e9e61a5d37e5c7
atucci <116757689+alessiotucci@users.noreply.github.com>
2024-09-22 15:36:38 +0200

    Update web_scraping.pl

--- web_scraping.pl
+++ web_scraping.pl
@@ -1,13 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
 use LWP::UserAgent;
+use HTTP::Request;
+use HTTP::Response;
+use HTML::TreeBuilder 5 -weak;  # Ensure weak references are used
 
 my $url = 'http://www.atac.roma.it';
+# prolly need  to add those
 my $ua = LWP::UserAgent->new;
-$ua->max_redirect( 7 ); # Set redirect limit
-my $response = $ua->get($url);
+$ua->timeout(10);
+$ua->agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36');
 
-if ($response->is_success) {
+$ua->max_redirect(7); # Set redirect limit
+#my $response = $ua->get($url);
+my $req = HTTP::Request->new(GET => $url);
+my $res = $ua->request($req);
+
+
+if ($res->is_success)
+{
 	    print $response->decoded_content;  # Output the page content
-} else {
+}
+else
+{
 	    die $response->status_line;
 }
 
